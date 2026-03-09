2e6c13a3eaa6ad0c4527df85c0dc7138e06c4778
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2026-03-08 18:13:08 -0700

    Lesson 36: Raging sea shading shader

---
+++ lessons/36-raging-sea-shading-shaders/src/shaders/water/vertex.glsl
@@ -0,0 +1,63 @@
+uniform float uTime;
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
+
+uniform float uSmallWavesElevation;
+uniform float uSmallWavesFrequency;
+uniform float uSmallWavesSpeed;
+uniform float uSmallIterations;
+uniform float uShift;
+
+varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/perlinClassic3D.glsl
+
+float waveElevation(vec3 position) {
+    float elevation = sin(position.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+                      sin(position.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+                      uBigWavesElevation;
+
+    for(float i = 1.0; i <= uSmallIterations; i++)
+    {
+        elevation -= abs(perlinClassic3D(vec3(position.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+    }
+
+    return elevation;
+}
+
+void main()
+{
+    // Base position
+    float shift = uShift; // how far the neighbors are going to be
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // vec3 modelPositionA = modelPosition.xyz;
+    // modelPositionA += shift;
+    vec3 modelPositionA = modelPosition.xyz + vec3(shift, 0.0, 0.0);
+    vec3 modelPositionB = modelPosition.xyz + vec3(0.0, 0.0, - shift); // shift is negative because of the cross product
+
+    // Elevation
+    float elevation = waveElevation(modelPosition.xyz);
+    modelPosition.y += elevation;
+    modelPositionA.y += waveElevation(modelPositionA);
+    modelPositionB.y += waveElevation(modelPositionB);
+    
+    // Compute normal
+    vec3 toA = normalize(modelPositionA - modelPosition.xyz);
+    vec3 toB = normalize(modelPositionB - modelPosition.xyz);
+    vec3 computedNormal = cross(toA, toB);
+
+    // Final position
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    // Varyings
+    vElevation = elevation;
+    // vNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
+    vNormal = computedNormal;
+    vPosition = modelPosition.xyz;
+}
